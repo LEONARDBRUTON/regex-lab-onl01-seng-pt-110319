b3a6d5d47c1a02676f84ca1ea34b68a2a94c46e4
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-29 02:18:11 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,15 +19,6 @@ def words_five_letters_long(text)
     array.grep(/\b\w{5}\b/)
 end
 
-def first_word_capitalized_and_ends_with_punctuatin?(text)
-   first_letter = text.scan(/\w+/)[0]
-  if (text.scan(/\./)) && (first_letter == first_letter.upcase)
-    true
-  else  
-    false
-  end
-end
-
 def first_word_capitalized_and_ends_with_punctuation_2?(text)
   if text.match(/^[A-Z]/) && text.match(/[\.]$/)
     true
